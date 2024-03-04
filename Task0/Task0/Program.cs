9dbf906ddcb200e168b6a36c5ee12f353ebf1442
247034 Aleksander Moszyński <247034@edu.p.lodz.pl>
2024-03-05 00:44:08 +0100

    Sample of compiled program and unit tests

---
+++ Task0/Task0/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Task0
+{
+    public class SumCalculator
+    {
+        public int CalculateSum(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            var calculator = new SumCalculator();
+            Console.WriteLine("Enter the first number:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int result = calculator.CalculateSum(number1, number2);
+            Console.WriteLine($"The sum of {number1} and {number2} is {result}.");
+        }
+    }
+}
